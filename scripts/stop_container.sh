228f88f043c52d207468c6943e03a7c597845467
devopsaziz <120872672+devopsaziz@users.noreply.github.com>
2025-05-19 02:19:40 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 set -e
 
-# Greeting
+# Stop the running container (if any)
 echo "Hi"
-
-# Get running container IDs
-running_containers=$(docker ps -q)
-
-if [ -z "$running_containers" ]; then
-  echo "No running containers found."
-else
-  echo "Stopping running containers..."
-  docker stop $running_containers
-
-  echo "Removing stopped containers..."
-  docker rm $running_containers
-
-  echo "All running containers have been stopped and removed."
-fi
