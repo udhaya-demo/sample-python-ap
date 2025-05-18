d530d53ac3ce8cb64d18258634a1d5e35da59a1e
devopsaziz <120872672+devopsaziz@users.noreply.github.com>
2025-05-19 01:32:46 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull kubeaziz/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 kubeaziz/simple-python-flask-app 
+docker run -d -p 8000:8000 kubeaziz/simple-python-flask-app 
