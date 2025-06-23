59bde4daa7cddf6b528efe62696907962695395f
udhaya-demo <ishwaryakshara@gmail.com>
2025-06-23 14:38:18 +0530

    Update start_container.sh
    updated start container script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+# Stop and remove any existing container based on this image
+CONTAINER_ID=$(docker ps -q --filter "ancestor=udhayademo/simple-python-flask-app")
+if [ ! -z "$CONTAINER_ID" ]; then
+    echo "Stopping and removing existing container..."
+    docker stop "$CONTAINER_ID"
+    docker rm "$CONTAINER_ID"
+fi
+
 # Pull the Docker image from Docker Hub
 docker pull udhayademo/simple-python-flask-app
 
