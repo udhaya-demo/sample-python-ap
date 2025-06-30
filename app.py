63b1e6796dbda0ff11b173ecdd3196f9d5038407
udhaya-demo <ishwaryakshara@gmail.com>
2025-06-30 14:04:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def home():
     return render_template('index.html', posts=posts)
     
     if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+         app.run(host='0.0.0.0', port=5000)
