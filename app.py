6d64314128151c5c36a9a0834cc45a3c5dbcc2fe
udhaya-demo <ishwaryakshara@gmail.com>
2025-06-30 13:15:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return "<h1>Welcome to Udhaya's Live Flask App!</h1><p>Running on Docker + AWS + HTTPS</p>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
