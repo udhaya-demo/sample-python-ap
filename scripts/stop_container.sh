1e3821193ed577342a60ec6b612892dfa64dd671
udhaya-demo <ishwaryakshara@gmail.com>
2025-07-01 13:39:44 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-set -e
+docker stop udhaya-feed-app || true
+docker rm udhaya-feed-app || true
 
-# Stop any running container based on the image
-CONTAINER_ID=$(docker ps -q --filter "ancestor=udhayademo/simple-python-flask-app")
-if [ ! -z "$CONTAINER_ID" ]; then
-    echo "Stopping container..."
-    docker stop "$CONTAINER_ID"
-    docker rm "$CONTAINER_ID"
-fi
