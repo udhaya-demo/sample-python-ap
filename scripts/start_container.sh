aca2f3fadaa4811eada3800ecca8592f1a86fe76
udhaya-demo <ishwaryakshara@gmail.com>
2025-07-01 14:04:30 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/app
 
-docker run -d -p 80:5000 --name udhaya-feed-app your-docker-username/udhaya-feed-app:latest
+docker run -d -p 80:5000 --name udhaya-feed-app udhayademo/udhaya-feed-app:latest
